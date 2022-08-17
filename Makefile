c20264e3cab652a336ae04b6802b486fccdda63d
malatini <malatini@student.42.fr>
2022-08-17 15:26:48 +0200

    cleaning files for master branch

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 all:
 			cd ./app && npm install && npm start
-
-buildImage:
-			docker build -t react-image . -f ./my-app/Dockerfile
--- app/src/components/Displaying/Recipe/RecipeItem/Step/Step.tsx
+++ app/src/components/Displaying/Recipe/RecipeItem/Step/Step.tsx
@@ -1,13 +1,12 @@
 import { StepProps } from "../../../../../interfaces/Recipe/Step/Step.interface";
 import React from "react";
 
-
 export default function Step(props: StepProps)
 {
   return (
     <>
       {props && props.list && props.list.map((step) =>
-          <span key={step.order} style={{fontSize: "12px"}}>{step.order} : {step.description}</span>
+          <span key={step.order} style={{fontSize: "12px", textAlign: "left", padding: "2px"}}>{step.order}. {step.description}</span>
       )}
     </>
   );
